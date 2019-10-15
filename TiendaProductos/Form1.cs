f576d0ecf1f243fc46087356fc330b2fa1383411
faraday58 <jarodriguezvera@gmail.com>
2019-10-15 17:34:19 -0500

    Programando cada botón según la selección

--- TiendaProductos/Form1.cs
+++ TiendaProductos/Form1.cs
@@ -11,25 +11,64 @@ namespace TiendaProductos
         private Arma arma;
         private List<Arma> armas;
         private float costo;
+        private byte indice;
         public Form1()
         {
             InitializeComponent();
             armas = new List<Arma>();
-            arma = new Arma("Shuriken", 25, 10, "Juguete para niños y no tan niños");
-            arma = new Arma("Uzi", 100, 5, "Réplica de plástico con estilo realista");
-            arma = new Arma("Kunai Lazer", 50, 5, "Se ilumina en la obscuridad");
+            
         }
 
         private void BtnComprarArmaBlanca_Click(object sender, EventArgs e)
         {
+            eligeCompra(sender);
             
-            armas.Add(arma);
-            lstbProductosSelect.Items.Add(armas[0].Nombre);
-            lbArmaBlanca.Text = armas[0].Descripcion;
-            lbArmaBlanca.Visible = true;
-            costo += armas[0].Precio;
-            armas[0].Stock -= 1;
-            lbCosto.Text = "$"+costo.ToString();
         }
+
+        private void eligeCompra(object sender)
+        {
+            Button botonPress =(Button)sender;
+
+            if (botonPress == btnComprarArmaBlanca)
+            {
+                armas.Add(new Arma("Shuriken", 25, 10, "Juguete para niños y no tan niños"));
+                lstbProductosSelect.Items.Add(armas[indice].Nombre);
+                lbArmaBlanca.Text = armas[indice].Descripcion;
+                lbArmaBlanca.Visible = true;
+                costo += armas[indice].Precio;
+                armas[indice].Stock -= 1;
+                lbCosto.Text = "$" + costo.ToString();
+                indice++;
+
+            }else if(botonPress == btnComprarArmaFuego)
+            {
+                armas.Add(new Arma("Uzi", 100, 5, "Réplica de plástico con estilo realista"));
+                lstbProductosSelect.Items.Add(armas[indice].Nombre);
+                lbArmaFuego.Text = armas[indice].Descripcion;
+                lbArmaFuego.Visible = true;
+                costo += armas[indice].Precio;
+                armas[indice].Stock -= 1;
+                lbCosto.Text = "$" + costo.ToString();
+                indice++;
+
+            }else if(botonPress == btnArmaLazer)
+            {
+                armas.Add(new Arma("Kunai Lazer", 50, 5, "Se ilumina en la obscuridad"));
+                lstbProductosSelect.Items.Add(armas[indice].Nombre);
+                lbArmaLazer.Text = armas[indice].Descripcion;
+                lbArmaLazer.Visible = true;
+                costo += armas[indice].Precio;
+                armas[indice].Stock -= 1;
+                lbCosto.Text = "$" + costo.ToString();
+                indice++;
+
+
+            }
+
+
+        }
+
+
+
     }
 }
